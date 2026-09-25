55b4ab1acc840986a82b31b60da309934b735f35
Petr Fedin <81327591+PetrFedin@users.noreply.github.com>
2026-09-26 02:22:22 +0300

    feat: load Taste Graph interaction layer

--- public/main.js
+++ public/main.js
@@ -8,6 +8,7 @@ import './modules/auction-results.js';
 import './modules/market-intelligence.js';
 import './modules/object-desire.js';
 import './modules/culture-discovery.js';
+import './modules/taste-signals.js';
 import './modules/inquiry.js';
 
 import './modules/services.js';
